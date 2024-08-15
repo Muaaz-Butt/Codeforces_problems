86cfa9aff2d0a0d7e890d02dbba671b474b0ab42
Muaaz Butt <muaazbutt585@gmail.com>
2024-08-15 11:04:35 +0500

    sort two numbers

---
+++ my_first_sorting_problem.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x, y;
+        cin >> x >> y;
+        if (x <= y){
+            cout << x << " " << y << "\n"; 
+        } else {
+            cout << y << " " << x << "\n";
+        }
+    }
+
+    return 0;
+}
