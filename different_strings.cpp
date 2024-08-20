fc31ccbac8971621b9538d527abdf4189078c2dd
Muaaz Butt <muaazbutt585@gmail.com>
2024-08-20 21:42:43 +0500

    if find different character swap it and return

--- different_strings.cpp
+++ different_strings.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
 #include <string>
-
+ 
 using namespace std;
-
-void solve() {
-    string s;
-    cin >> s;
-    bool ok = false;
-    
-    for (int i = 1; i < s.length(); i++) {
-        if (s[i] != s[0]) {
-            swap(s[0], s[i]);
-            ok = true;
-            break;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        int len = s.length();
+        bool flag = true;
+        for(int i = 1; i < len && flag; i++){
+            if (s[i] != s[i - 1]){
+                char temp = s[i];
+                s[i] = s[i - 1];
+                s[i - 1] = temp;
+                cout << "YES\n" << s << "\n"; 
+                flag = false;
+            }
+        }
+        if (flag) {
+            cout << "NO\n";
         }
     }
-    
-    if (!ok) {
-        cout << "NO" << endl;
-        return;
-    }
-    
-    cout << "YES" << endl;
-    cout << s << endl;
-}
-
-int main() {
-    int tt;
-    cin >> tt;
-    while (tt--) {
-        solve();
-    }
-    
+ 
     return 0;
 }
