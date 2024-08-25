b8b614594a4999b2ac427495e06a3ac49138af56
Muaaz Butt <muaazbutt585@gmail.com>
2024-08-25 16:04:05 +0500

    simple coindtional problem

---
+++ Stair, Peak, or Neither?.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a < b && b < c) {
+        cout << "STAIR\n";
+        return;
+    } else if (a < b && b > c) {
+        cout  << "PEAK\n";
+    } else cout << "NONE\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
