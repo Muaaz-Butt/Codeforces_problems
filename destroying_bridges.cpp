7ec44a4d67786bb723fd1acce8ba47c1700c1c1d
Muaaz Butt <muaazbutt585@gmail.com>
2024-08-28 17:01:55 +0500

    destoy k bridge

---
+++ destroying_bridges.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    if (k >= n - 1)
+        cout << "1\n";
+    else
+        cout << n << "\n";
+}
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
