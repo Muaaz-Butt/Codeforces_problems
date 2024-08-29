6c33a4ada6bdd852e93d7a5837b9a4d72f8c7424
Muaaz Butt <muaazbutt585@gmail.com>
2024-08-29 18:15:46 +0500

    simple bitmasking problem

---
+++ odd_one_out.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << (a^b^c) << "\n";
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
